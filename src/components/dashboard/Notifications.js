4265f63fa0bdfe79ec777843746114b734f3bac9
Kat Grennan <caitlyn.grennan@techtonic.com>
2019-07-19 12:29:09 -0600

    Limit height of lists, fix brand logo, customize scrollbars, remove notification limit

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -6,16 +6,16 @@ const Notifications = (props) => {
 
     return(
         <div className="section">
-            <div className="card z-depth-0">
+            <div className="card note-card z-depth-0">
                 <div className="card-content">
                     <span className="card-title">Notifications</span>
                     <ul className="notifications">
                         { notifications && notifications.map(item => {
                             return (
                                 <li key={item.id}>
-                                    <span className="pink-text">{item.user} </span>
+                                    <span className="note-author">{item.user} </span>
                                     <span>{ item.content }</span>
-                                    <div className="grey-text note-date">
+                                    <div className="note-date">
                                         { moment(item.time.toDate()).fromNow() }
                                     </div>
                                 </li>
